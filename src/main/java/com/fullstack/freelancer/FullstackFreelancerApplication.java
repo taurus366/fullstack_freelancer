cfc0f4b480af6249d8aad8a4c1bdb086f7af3d83
Arcerr <taurus..ali47>
2023-04-14 14:31:31 +0300

    .

---
+++ src/main/java/com/fullstack/freelancer/FullstackFreelancerApplication.java
@@ -0,0 +1,13 @@
+package com.fullstack.freelancer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackFreelancerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullstackFreelancerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fullstack/freelancer/FullstackFreelancerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullstack.freelancer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackFreelancerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
